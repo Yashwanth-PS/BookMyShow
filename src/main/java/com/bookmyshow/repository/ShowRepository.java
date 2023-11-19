bb1e4ddfaa1d742daeed6bf49c78eabe6877d18e
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-19 22:44:58 +0530

    Book My Show - Created the Show Repository

---
+++ src/main/java/com/bookmyshow/repository/ShowRepository.java
@@ -0,0 +1,17 @@
+package com.bookmyshow.repository;
+
+import com.Project.BookMyShow.models.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ShowRepository extends JpaRepository<Show, Long> {
+    // Optional is like a placeholder, It might have the object, or it might be Empty
+    // If the data is not found, It wouldn't be null instead it will be Empty
+    @Override
+    Optional<Show> findById(Long showId);
+    @Override
+    Show save(Show show);
+}
