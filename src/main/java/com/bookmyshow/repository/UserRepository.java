e79a0ac1486050d48876d5fa097f74c99a8aedf8
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-22 21:21:06 +0530

    Book My Show - Created the User Repository

---
+++ src/main/java/com/bookmyshow/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.bookmyshow.repository;
+
+import com.bookmyshow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    Optional<User> findById(Long userId);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndName(String email, String name);
+    User save(User user);
+    // User findByEmail(String email);
+}
