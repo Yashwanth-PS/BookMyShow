5b5aba37338974dc13af9ad4fdbb7167e69c22ac
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-24 13:23:08 +0530

    Book My Show - Created the InvalidCredentialException

--- src/main/java/com/bookmyshow/service/UserServiceImpl.java
+++ src/main/java/com/bookmyshow/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bookmyshow.service;
 
+import com.bookmyshow.exception.InvalidCredentialException;
 import com.bookmyshow.exception.UserAlreadyExistsException;
 import com.bookmyshow.model.User;
 import com.bookmyshow.repository.UserRepository;
