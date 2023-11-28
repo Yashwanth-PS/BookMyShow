efb62e0085fb0fa0ec2df1674c2986fafd513969
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2023-11-28 21:43:59 +0530

    Book My Show - Created the Ticket Service

---
+++ src/main/java/com/bookmyshow/service/TicketService.java
@@ -0,0 +1,16 @@
+package com.bookmyshow.service;
+
+import com.bookmyshow.model.Ticket;
+
+import java.util.List;
+
+public interface TicketService {
+    Ticket bookTicket(Long userId, List<Long> showSeatIds, Long showId) throws ShowSeatNotAvailableException;
+
+    Ticket getTicket(Long ticketId) throws TicketNotFoundException;
+
+    Ticket cancelTicket(Long ticketId);
+
+    Ticket transferTicket(Long ticketId, Long fromUserId, Long toUserId) throws TicketNotFoundException, UserNotFoundException;
+
+}
