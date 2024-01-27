247a30cdf7df1389be4684ce9d51f2f1f9e537ab
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-01-27 18:34:24 +0530

    BookMyShow - Implemented BCrypt for Password Encryption

--- src/main/java/com/bookmyshow/service/UserServiceImpl.java
+++ src/main/java/com/bookmyshow/service/UserServiceImpl.java
@@ -6,26 +6,28 @@ import com.bookmyshow.exception.UserNotFoundException;
 import com.bookmyshow.model.User;
 import com.bookmyshow.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User login(String email, String password) {
         Optional<User> optionalUser = userRepository.findByEmail(email);
         User user = optionalUser.orElseThrow(() -> new UserNotFoundException("User with given email does not exist: " + email));
-        if (Objects.equals(user.getPassword(), password)) {
+        if (passwordEncoder.matches(user.getPassword(), password)) {
             return user;
         } else {
             throw new InvalidCredentialException("Invalid credentials for user with email: " + email);
@@ -36,7 +38,7 @@ public class UserServiceImpl implements UserService {
         Optional<User> optionalUser = userRepository.findByEmail(email);
         optionalUser.ifPresent(u -> {throw new UserAlreadyExistsException("User with given email already exists: " + email);});
         User newUser = new User();
-        newUser.setPassword(password);
+        newUser.setPassword(passwordEncoder.encode(password));
         newUser.setName(name);
         newUser.setEmail(email);
         return userRepository.save(newUser);
