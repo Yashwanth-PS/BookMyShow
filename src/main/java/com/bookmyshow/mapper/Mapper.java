54b24ead70c1c332bff476748a069f9791ed2ddb
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-01 17:37:08 +0530

    BookMyShow - Updated the Mapper

--- src/main/java/com/bookmyshow/mapper/Mapper.java
+++ src/main/java/com/bookmyshow/mapper/Mapper.java
@@ -1,9 +1,31 @@
 package com.bookmyshow.mapper;
 
+import com.bookmyshow.dto.TicketResponseDTO;
 import com.bookmyshow.dto.UserSignUpResponseDTO;
+import com.bookmyshow.model.Ticket;
 import com.bookmyshow.model.User;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Mapper { // TODO: create a user object and send the details
+    public static TicketResponseDTO convertTicketToTicketResponseDTO(Ticket ticket) {
+        TicketResponseDTO ticketResponse = new TicketResponseDTO();
+        // Set ticketResponse properties based on the booked ticket
+        ticketResponse.setShowTime(ticket.getShow().getStartTime());
+        ticketResponse.setMovieName(ticket.getShow().getMovie().getName());
+        ticketResponse.setTotalAmount(ticket.getTotalAmount());
+        ticketResponse.setAuditoriumName(ticket.getShow().getAuditorium().getName());
+        /* List<String> seatNumber = new ArrayList<>();
+        for (ShowSeat seat : ticket.getShowSeats()) {
+            seatNumber.add(seat.getSeat().getSeatNumber());
+        } */
+        List<String> seatNumber = ticket.getShowSeats().stream()
+                .map(showSeat -> showSeat.getSeat().getSeatNumber())
+                .collect(Collectors.toList());
+        ticketResponse.setSeatNumbers(seatNumber);
+        return ticketResponse;
+    }
     public static UserSignUpResponseDTO convertUserToUserResponseDTO(User user) {
         UserSignUpResponseDTO userSignUpResponseDTO = new UserSignUpResponseDTO();
         // userSignUpResponseDTO.setId(user.getId());
