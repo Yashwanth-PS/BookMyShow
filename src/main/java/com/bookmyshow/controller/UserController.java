5e0c5d0090cd01b56ed5ac25814c1834a11d106e
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-02 22:21:50 +0530

    BookMyShow - Updated the User Controller

--- src/main/java/com/bookmyshow/controller/UserController.java
+++ src/main/java/com/bookmyshow/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.bookmyshow.controller;
 
-import com.bookmyshow.controller.utils.ControllerUtil;
 import com.bookmyshow.dto.UserSignInRequestDTO;
 import com.bookmyshow.dto.UserSignInResponseDTO;
 import com.bookmyshow.dto.UserSignUpRequestDTO;
 import com.bookmyshow.dto.UserSignUpResponseDTO;
-import com.bookmyshow.mapper.Mapper;
 import com.bookmyshow.model.User;
 import com.bookmyshow.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,49 +14,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.bookmyshow.controller.utils.ControllerUtil.validateUserSignUpRequestDTO;
+import static com.bookmyshow.mapper.UserMapper.*;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<UserSignUpResponseDTO> signUp(@RequestBody UserSignUpRequestDTO userSignUpRequestDTO) {
-        UserSignUpResponseDTO responseDTO = new UserSignUpResponseDTO();
+        UserSignUpResponseDTO responseDTO;
         try {
-            ControllerUtil.validateUserSignUpRequestDTO(userSignUpRequestDTO);
-            User user = userService.signUp(
-                    userSignUpRequestDTO.getName(),
-                    userSignUpRequestDTO.getEmail(),
-                    userSignUpRequestDTO.getPassword()
-            );
-            responseDTO = Mapper.convertUserToUserResponseDTO(user);
+            validateUserSignUpRequestDTO(userSignUpRequestDTO);
+            User user = userService.signUp(userSignUpRequestDTO);
+            responseDTO = convertUserToUserSignUpResponseDTO(user);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
-            responseDTO.setResponseCode(500);
-            responseDTO.setResponseMessage("Please Use a Valid Email and Password");
-            return ResponseEntity.status(500).body(responseDTO);
+            log.error("Please enter Valid Email and Password Format");
+            return ResponseEntity.badRequest().build();
         }
     }
 
     @PostMapping("/signin")
     public ResponseEntity<UserSignInResponseDTO> signIn(@RequestBody UserSignInRequestDTO userSignInRequestDTO) {
-        UserSignInResponseDTO responseDTO = new UserSignInResponseDTO();
+        UserSignInResponseDTO responseDTO;
         try { // You may need to implement the actual sign-in logic based on your requirements
-            User user = userService.login(userSignInRequestDTO.getEmail(),
-                                          userSignInRequestDTO.getPassword());
-            responseDTO.setName(user.getName());
-            responseDTO.setEmail(user.getEmail());
-            responseDTO.setResponseCode(200);
-            responseDTO.setResponseMessage("SUCCESS");
+            User user = userService.login(userSignInRequestDTO);
+            responseDTO = convertUserToUserSignInResponseDTO(user);
             return ResponseEntity.ok(responseDTO);
         } catch (Exception e) {
-            responseDTO.setName("User Not Found");
-            responseDTO.setEmail("User Not Found");
-            responseDTO.setResponseCode(401); // Unauthorized status code
-            responseDTO.setResponseMessage("Invalid credentials");
-            return ResponseEntity.status(401).body(responseDTO);
+            log.error("Invalid Email and Password");
+            return ResponseEntity.status(401).build(); // Unauthorized status code
         }
     }
 }
