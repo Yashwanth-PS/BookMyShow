ce28b7700cfe24e3f66c9fb4e0442ab6d60c4cd0
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-02-02 22:20:13 +0530

    BookMyShow - Updated the User Service Interface

--- src/main/java/com/bookmyshow/service/UserService.java
+++ src/main/java/com/bookmyshow/service/UserService.java
@@ -1,8 +1,10 @@
 package com.bookmyshow.service;
 
+import com.bookmyshow.dto.UserSignInRequestDTO;
+import com.bookmyshow.dto.UserSignUpRequestDTO;
 import com.bookmyshow.model.User;
 
 public interface UserService {
-    User login(String email, String password);
-    User signUp(String name, String email, String password);
+    User login(UserSignInRequestDTO userSignInRequestDTO);
+    User signUp(UserSignUpRequestDTO userSignUpRequestDTO);
 }
